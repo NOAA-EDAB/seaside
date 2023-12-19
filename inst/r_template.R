349da7738a2f9e8d78cfe5b95971a73ab762456b
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2023-12-19 16:24:18 -0500

    create rproj, add sample code template

---
+++ inst/r_template.R
@@ -0,0 +1,24 @@
+
+# Metadata ----
+
+### Project name:
+### Code purpose:
+
+### Author:
+### Date started:
+
+### Code reviewer:
+### Date reviewed:
+
+# Libraries & functions ----
+
+# Data ----
+
+# Analyses ----
+
+## Create a subsection for each analysis ----
+
+## If it is a very long analysis, create multiple subsections ----
+
+### Mark lines where outputs are saved ----
+
